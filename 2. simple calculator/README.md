7cd267677b57d51e46050fb1690a16ad71feac38
Alien-LK48 <myworktime2022@gmail.com>
2025-01-24 17:43:43 +0600

    calculator

--- 2. simple calculator/README.md
+++ 2. simple calculator/README.md
@@ -1,8 +1,51 @@
-# React + Vite
+# Counter App
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+A simple **Calculator App** built using **React.js** and styled with **Tailwind CSS**.
 
-Currently, two official plugins are available:
+## Features
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- **Calculates based on operations**: Value will add, subtract, multiple and division.
+- **User Input Number**: Allow users to directly input two custom numbers.
+- **Clear**: Clear everything after calculating.
+
+## Technologies Used
+
+- **React.js**: For building the user interface and handling state management.
+- **Tailwind CSS**: For styling and designing a responsive layout.
+
+## Installation
+
+1. Clone the repository:
+   ```bash
+   git clone <repository-url>
+   ```
+
+2. Navigate to the project directory:
+   ```bash
+   cd counter-app
+   ```
+
+3. Install dependencies:
+   ```bash
+   npm install
+   ```
+
+4. Start the development server:
+   ```bash
+   npm run dev
+   ```
+
+## Usage
+
+1. Use the **+**, **-** , **x**, **÷** buttons to perform operations.
+2. Enter two custom number in the input field.
+
+## Live Demo
+
+Check out the live project here: [Counter App](https://r-counter-app.netlify.app/)
+
+## Acknowledgments
+
+- **React.js Documentation**: [https://reactjs.org/docs](https://reactjs.org/docs)
+- **Tailwind CSS Documentation**: [https://tailwindcss.com/docs](https://tailwindcss.com/docs)
+
--- 2. simple calculator/src/App.jsx
+++ 2. simple calculator/src/App.jsx
@@ -1,12 +1,58 @@
 import React, { useState } from 'react'
-import Body from './components/Body'
 console.clear()
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [inpONe, setInpONe] = useState(``)
+  const valueOfOneInp = (e) => {
+    setInpONe(e.target.value)
+  }
+  const [inpTwo, setInpTwo] = useState(``)
+  const valueOfTwoInp = (e) => {
+    setInpTwo(e.target.value)
+  }
+  const [result, setResult] = useState(``)
+  const showResult = (opr) => {
+    if (opr === `+`) {
+      const add = Number(inpONe) + Number(inpTwo)
+      setResult(add.toFixed(2))
+    }
+    if (opr === `-`) {
+      const sub = Number(inpONe) - Number(inpTwo)
+      setResult(sub.toFixed(2))
+    }
+    if (opr === `*`) {
+      const mul = Number(inpONe) * Number(inpTwo)
+      setResult(mul.toFixed(2))
+    }
+    if (opr === `/`) {
+      const div = Number(inpONe) / Number(inpTwo)
+      setResult(div.toFixed(2))
+    }
+
+  }
+  const clearDisplay = () => {
+    setResult(``)
+    setInpONe(``)
+    setInpTwo(``)
+  }
   return (
     <div className='flex flex-col items-center justify-center gap-[30px]'>
-      <h1 className='text-4xl text-center uppercase mt-[100px]'>counter app</h1>
-      <Body count={count} setCount={setCount}/>
+      <h1 className='text-4xl text-center uppercase mt-[100px]'>simple calculator</h1>
+      <div className='bg-[black] w-[250px] h-[270px] flex flex-col p-[20px]'>
+        <div className='flex flex-row gap-[10px] items-center justify-center'>
+          <input type="text" className='w-[90px] h-[35px] text-center' placeholder='input 1' value={inpONe} onChange={valueOfOneInp} /> <br />
+          <input type="text" className='w-[90px] h-[35px] text-center' placeholder='input 2' value={inpTwo} onChange={valueOfTwoInp} />
+        </div>
+        <p className='text-center font-bold text-3xl text-[white] my-[20px] px-[20px]'>{result}</p>
+        <div className='flex flex-row gap-[5px] justify-center items-center'>
+          <button className='btn' onClick={() => { showResult(`+`) }}>+</button>
+          <button className='btn' onClick={() => { showResult(`-`) }}>-</button>
+          <button className='btn' onClick={() => { showResult(`*`) }}>x</button>
+          <button className='btn' onClick={() => { showResult(`/`) }}>÷</button>
+        </div> <br />
+        <button className='btn bg-[red]' onClick={clearDisplay}>Clear</button>
+      </div>
+
     </div>
   )
 }
